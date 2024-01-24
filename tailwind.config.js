1bcb29ec7cc179694b6760d2efb8fcff852f65f5
Florence <fhinder@gmail.com>
2024-01-24 15:43:21 +0000

    new FLEXSTA landing page + stripe link

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "index.html", // Adjust if your HTML files are located elsewhere
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
